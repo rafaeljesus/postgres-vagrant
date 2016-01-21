3d048880157f59403aa99dfad3ee64eeb9cd2b12
Rafael Jesus <rafaelljesus86@gmail.com>
2016-01-20 23:06:54 -0200

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -9,11 +9,7 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 
 apt-get update
 apt-get -y install postgresql-9.4 postgresql-contrib postgresql-client
-sudo -u postgres createdb estate
-sudo -u postgres createdb estate_development
-sudo -u postgres createdb estate_test
 
 sed -i "/#listen_addresses /s/^/listen_addresses = '*' /" /etc/postgresql/9.4/main/postgresql.conf
 echo "host  all   all   10.0.2.2/24     trust" >> /etc/postgresql/9.4/main/pg_hba.conf
-sudo -u postgres createuser -S -D -R -e estate
 service postgresql restart
